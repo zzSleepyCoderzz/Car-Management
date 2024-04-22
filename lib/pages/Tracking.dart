282c41beb48511b90defd4becd2f056c6c5991a8
zzSleepyCoderzz <73248416+zzSleepyCoderzz@users.noreply.github.com>
2024-04-22 19:00:57 +0800

    Added Google Maps

--- lib/pages/Tracking.dart
+++ lib/pages/Tracking.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrackingPage extends StatefulWidget {
   const TrackingPage({super.key});
@@ -9,6 +11,13 @@ class TrackingPage extends StatefulWidget {
 }
 
 class _TrackingPageState extends State<TrackingPage> {
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +25,32 @@ class _TrackingPageState extends State<TrackingPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Tracking Page'),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20), // Adjust as needed
+            ),
+            clipBehavior: Clip.antiAlias, // This line is important
+            width: MediaQuery.of(context).size.height * 0.4,
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition:
+                  CameraPosition(target: _center, zoom: 11.0),
+              myLocationButtonEnabled: true,
+              myLocationEnabled: true,
+              zoomControlsEnabled: true,
+              markers: {
+                const Marker(
+                  markerId: MarkerId('1'),
+                  position: LatLng(45.521563, -122.677433),
+                  infoWindow: InfoWindow(
+                    title: 'Portland, OR',
+                    snippet: '5 Star Rating',
+                  ),
+                ),
+              },
+            ),
+          ),
         ],
       ),
     ));
