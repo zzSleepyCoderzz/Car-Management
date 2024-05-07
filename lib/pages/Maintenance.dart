5afe61b14afaa711d3b0bf63cd3a84674ae735a9
Timothy Chin <timothycgy6@gmail.com>
2024-05-07 23:36:41 +0800

    Dropdown Menu for Maintenance Page

--- lib/pages/Maintenance.dart
+++ lib/pages/Maintenance.dart
@@ -1,5 +1,6 @@
 import 'package:car_management/components/button.dart';
 import 'package:flutter/material.dart';
+import 'package:car_management/components/globals.dart' as globals;
 
 class MaintenancePage extends StatefulWidget {
   const MaintenancePage({super.key});
@@ -9,6 +10,9 @@ class MaintenancePage extends StatefulWidget {
 }
 
 class _MaintenancePageState extends State<MaintenancePage> {
+
+  //Dropdown to choose car
+  String dropdownValue = globals.carData['Car1']['Car Model'];
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +32,35 @@ class _MaintenancePageState extends State<MaintenancePage> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                //Dropdown to choose car
+                Text('Choose a car:')
+,                DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: <String>[globals.carData['Car1']['Car Model'], globals.carData['Car2']['Car Model'], globals.carData['Car3']['Car Model']]
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+
+
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
                 MaintenanceButton(
                     onTap: () {
                       Navigator.pushNamed(context, '/service_history');
